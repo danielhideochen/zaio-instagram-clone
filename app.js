029ce0250fc42e97f3cd9aaa21e71ff9d30433ff
[Daniel Chen] <[danielhideochen@gmail.com]>
2023-07-11 00:33:35 -0700

    added create, read, and update(edit) posts functionality with buttons

--- app.js
+++ app.js
@@ -1,3 +1,47 @@
+const searchInput = document.querySelector(".search-input");
+const postsDiv = document.querySelector(".posts");
+
+const usernameInput = document.querySelector("#username");
+const imageLinkInput = document.querySelector("#imagelink");
+const captionInput = document.querySelector("#caption");
+const createPostBtn = document.querySelector("#create-post-btn");
+const editPostBtn = document.querySelector("#edit-post-btn");
+editPostBtn.style.display = "none";
+
+const editBtn = document.querySelector("#edit-btn");
+
+//access bootstrap functions to programatically use modals within my javascript
+const modal = new bootstrap.Modal(document.getElementById('modal'));
+const showCreateModal = document.querySelector("#show-create-modal");
+
+//event listeners
+searchInput.addEventListener("click", () => {
+    console.log(searchInput.value);
+})
+
+createPostBtn.addEventListener("click", () => {
+    console.log("create post btn clicked");
+    createPost(imageLinkInput.value, captionInput.value, usernameInput.value);
+})
+
+//we not have access to these functions
+showCreateModal.addEventListener("click", () => {
+    isEditMode = false;
+    createPostBtn.style.display = "block";
+    editPostBtn.style.display = "none";
+    usernameInput.value = "";
+    imageLinkInput.value = "";
+    captionInput.value = "";
+    modal.show();
+});
+editPostBtn.addEventListener("click", () => {
+    console.log("edit post btn clicked");
+    updatePost(postToEditId, imageLinkInput.value, captionInput.value);
+    modal.hide();
+});
+
+
+
 const outputPostStatus = (post) => {
     const output = `
     POST INFO:
@@ -10,8 +54,24 @@ const outputPostStatus = (post) => {
   };
 
 
-//spread and rest operators
-var feed = []
+//global variables
+var feed = [
+    {
+    id: 0,
+    username: "danielchen",
+    imageLink: "https://travel.home.sndimg.com/content/dam/images/travel/fullset/2015/08/03/america-the-beautiful-ss/adirondack-park-new-york-state.jpg.rend.hgtvcom.616.462.suffix/1491580836599.jpeg",
+    caption: "so beautiful",
+    likes: 0,
+    comments: [],
+    shares: 0,
+    isPublic: true,
+    createdAt: new Date(),
+    
+},
+];
+
+var isEditMode = false;
+var postToEditId = null;
 
     //create functionality
 const createPost = (imageLink, caption, username) => {
@@ -27,18 +87,33 @@ const createPost = (imageLink, caption, username) => {
         createdAt: new Date(),
         
     }
+    console.log("FEED", feed);
     feed.push(newPost);
-    //outputFeed(feed);
+    outputFeed();
+    modal.hide();
 };
 
 //read functionality
-const outputFeed = (feed) => {
-    const output = feed.map((p) => {
-        console.log(outputPostStatus(p));
-        return p;
-    })
-    console.log("NEW MAPPED ARRAY: ", output);
-}
+const outputFeed = () => {
+    const updatedFeed = feed.map((post) => {
+        return `
+        <div class="post-box">
+
+        <div class="post-header">
+            <p>${post.username}</p>
+            <button class="btn btn-sm btn-primary" onclick="showEditPostModal(${post.id})" >Edit</button>
+        </div>
+        <div class="post-image">
+            <img src="${post.imageLink}" alt="">
+        </div>
+        <div class="caption">
+            <p>${post.caption}</p>
+        </div>
+        </div>
+        `;
+    });
+    postsDiv.innerHTML = updatedFeed.join(" ");
+};
 
 const updatePost = (id, newImageLink, newCaption) => {
     const updatedFeed = feed.map((post) => {
@@ -49,6 +124,7 @@ const updatePost = (id, newImageLink, newCaption) => {
         return post;
     })
     feed = updatedFeed;
+    outputFeed();
     
 }
 
@@ -59,26 +135,41 @@ const deletePost = (id) => {
         }
     });
     feed = updatedFeed;
-    outputFeed(feed);
+    // outputFeed(feed);
+}
+
+const showEditPostModal = (id) => {
+    postToEditId = id;
+    isEditMode = true;
+    createPostBtn.style.display = "none";
+    editPostBtn.style.display = "block";
+    const postToEdit = feed[id];
+    console.log("postToEdit", postToEdit);
+    usernameInput.value = postToEdit.username;
+    imageLinkInput.value = postToEdit.imageLink;
+    captionInput.value = postToEdit.caption;
+    modal.show();
 }
 
 
-createPost("https://travel.home.sndimg.com/content/dam/images/travel/fullset/2015/08/03/america-the-beautiful-ss/adirondack-park-new-york-state.jpg.rend.hgtvcom.616.462.suffix/1491580836599.jpeg", 
-"so beautiful", 
-"danniel_chenn");
-createPost("https://travel.home.sndimg.com/content/dam/images/travel/fullset/2015/08/03/america-the-beautiful-ss/adirondack-park-new-york-state.jpg.rend.hgtvcom.616.462.suffix/1491580836599.jpeg", 
-"yo", 
-"cody");
-createPost("https://travel.home.sndimg.com/content/dam/images/travel/fullset/2015/08/03/america-the-beautiful-ss/adirondack-park-new-york-state.jpg.rend.hgtvcom.616.462.suffix/1491580836599.jpeg", 
-"hey", 
-"derek");
-createPost("https://travel.home.sndimg.com/content/dam/images/travel/fullset/2015/08/03/america-the-beautiful-ss/adirondack-park-new-york-state.jpg.rend.hgtvcom.616.462.suffix/1491580836599.jpeg", 
-"mountains", 
-"isaiah");
-createPost("https://travel.home.sndimg.com/content/dam/images/travel/fullset/2015/08/03/america-the-beautiful-ss/adirondack-park-new-york-state.jpg.rend.hgtvcom.616.462.suffix/1491580836599.jpeg", 
-"vacation", 
-"johnny");
+// createPost("https://travel.home.sndimg.com/content/dam/images/travel/fullset/2015/08/03/america-the-beautiful-ss/adirondack-park-new-york-state.jpg.rend.hgtvcom.616.462.suffix/1491580836599.jpeg", 
+// "so beautiful", 
+// "danniel_chenn");
+// createPost("https://travel.home.sndimg.com/content/dam/images/travel/fullset/2015/08/03/america-the-beautiful-ss/adirondack-park-new-york-state.jpg.rend.hgtvcom.616.462.suffix/1491580836599.jpeg", 
+// "yo", 
+// "cody");
+// createPost("https://travel.home.sndimg.com/content/dam/images/travel/fullset/2015/08/03/america-the-beautiful-ss/adirondack-park-new-york-state.jpg.rend.hgtvcom.616.462.suffix/1491580836599.jpeg", 
+// "hey", 
+// "derek");
+// createPost("https://travel.home.sndimg.com/content/dam/images/travel/fullset/2015/08/03/america-the-beautiful-ss/adirondack-park-new-york-state.jpg.rend.hgtvcom.616.462.suffix/1491580836599.jpeg", 
+// "mountains", 
+// "isaiah");
+// createPost("https://travel.home.sndimg.com/content/dam/images/travel/fullset/2015/08/03/america-the-beautiful-ss/adirondack-park-new-york-state.jpg.rend.hgtvcom.616.462.suffix/1491580836599.jpeg", 
+// "vacation", 
+// "johnny");
 
-updatePost(2, "www.imageLink", "SpaceX next alnch soon!");
-deletePost(4);
-deletePost(0);
+// updatePost(2, "www.imageLink", "SpaceX next alnch soon!");
+// deletePost(4);
+// deletePost(0);
+
+outputFeed();
